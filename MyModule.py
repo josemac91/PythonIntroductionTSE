1d94211f04e11d18e0f6ad3d563da532b83427d0
Jose Alvarez <josemanu.16ac@gmail.com>
2016-12-10 14:41:57 +0100

    New round of Python exercises

---
+++ MyModule.py
@@ -0,0 +1,5 @@
+class Animal(object):
+    def __init__(self, name):
+        self.name = name
+    def printName(self):
+        print(self.name)
--- NewFile.py
+++ NewFile.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import pandas as pd
+s = pd.Series([1, 3, 5])
+print(s)
+
+from MyModule import Animal
+lion = Animal("george")
+lion.printName()
+
