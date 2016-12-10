9667ce3a03a22f713ca48f406d7d24e526662d33
Jose Alvarez <josemanu.16ac@gmail.com>
2016-12-10 15:32:49 +0100

    Almost had Ex1, didn't try Ex2. See solutions

--- ExercisesSession2.py
+++ ExercisesSession2.py
@@ -1,2 +1,19 @@
-print("Here we go again")
-print("this time with some pushing...")
+# Exercise 1)
+
+number_list = [8, 2, 3]
+print(sorted(number_list))
+print(len(number_list))
+
+if len(number_list) < 1:
+    return number_list
+if len(number_list) % 2 == 0:
+    return float(sum(len(sorted(number_list))/2 - 1 : len(sorted(number_list))/2 + 1)))
+else:
+    return 1
+
+# see solutions
+
+# Exercise 2)
+
+# see solutions
+
